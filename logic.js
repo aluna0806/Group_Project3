c6c287e663e5649ab735aef9c34742a2cc10746a
DuvanDiaz <DuvanDiaz@users.github.com>
2020-09-12 12:28:00 -0500

    data

--- logic.js
+++ logic.js
@@ -1,31 +1,73 @@
-mapboxgl.accessToken = API_KEY ;
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-115.172813,  36.114647], // starting position [lng, lat]
-    zoom: 10 // starting zoom
+
+// Define Variable for Tile Layer
+
+var streetsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
+
+// Initialize LayerGroups 
+ var layers = {
+     onestar: new L.LayerGroup(),
+    //  twostars: new L.LayerGroup(),
+    //  threestars: new L.LayerGroup(),
+    //  fourstars: new L.LayerGroup(),
+    //  fivestars: new L.LayerGroup()
+ };
+
+ // Create Map
+var myMap = L.map("map", {
+    center: [36.114647, -115.172813], // Starting at Las Vegas
+    zoom: 5,
+    layers: [
+        layers.onestar,
+        // layers.twostars,
+        // layers.threestars,
+        // layers.fourstars,
+        // layers.fivestars
+    ]
 });
 
-var maxValue = ;
-var data = [ 
+// Add tileLayer
+streetsMap.addTo(myMap);
 
-// map.on('load', function () {
-    // // add source and layer for museums
-    //     map.addSource('museums', {
-    //     type: 'vector',
-    //     url: 'mapbox://mapbox.2opop9hr'
-    //     });
-    //     map.addLayer({
-    //     'id': 'museums',
-    //     'type': 'circle',
-    //     'source': 'museums',
-    //     'layout': {
-    // // make layer visible by default
-    //     'visibility': 'visible'
-    //     },
-    //     'paint': {
-    //     'circle-radius': 8,
-    //     'circle-color': 'rgba(55,148,179,1)'
-    //     },
-    //     'source-layer': 'museum-cusco'
-    //     });   
+
+// Define OverlayMaps
+var overlayMaps = {
+    "One Star Restaurants": layers.onestar,
+    // "Two Stars Restaurants": layers.twostars,
+    // "Three Stars Restaurants": layers.threestars,
+    // "Four Stars Restaurants": layers.fourstars,  
+    // "Five Stars Restaurants": layers.fivestars
+};
+
+
+// Layer Control 
+L.control.layers(null, overlayMaps).addTo(myMap);
+
+// Legend
+
+// Call Data and Start Functions Setup
+d3.csv("yelpdata.csv", function (error, data) {
+    console.log(data)
+    data.forEach(data => {
+        var layer_star
+        if (data.stars >= 1 & data.stars < 2){
+            layer_star = "onestar"
+        }
+
+        var newMarker = L.marker([data.latitude, data.longitude]);
+
+        // Add the new marker to the appropriate layer
+        newMarker.addTo(layers[layer_star]);
+
+        // Bind a popup to the marker that will  display on click. This will be rendered as HTML
+        newMarker.bindPopup("");
+    }); 
+});
+
+    // d3.csv("Data/Yelp_InitialScrub.csv", function (//city or rating))
+
+
