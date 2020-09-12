6e586d3fe11e8e2b6fe6122bf9f1e06f795ea287
DuvanDiaz <DuvanDiaz@users.github.com>
2020-09-12 14:10:18 -0500

    css

--- logic.js
+++ logic.js
@@ -1,73 +1,74 @@
-
-// Define Variable for Tile Layer
-
-var streetsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY
-  });
-
-// Initialize LayerGroups 
- var layers = {
-     onestar: new L.LayerGroup(),
-    //  twostars: new L.LayerGroup(),
-    //  threestars: new L.LayerGroup(),
-    //  fourstars: new L.LayerGroup(),
-    //  fivestars: new L.LayerGroup()
- };
-
- // Create Map
-var myMap = L.map("map", {
-    center: [36.114647, -115.172813], // Starting at Las Vegas
-    zoom: 5,
-    layers: [
-        layers.onestar,
-        // layers.twostars,
-        // layers.threestars,
-        // layers.fourstars,
-        // layers.fivestars
-    ]
-});
-
-// Add tileLayer
-streetsMap.addTo(myMap);
-
-
-// Define OverlayMaps
-var overlayMaps = {
-    "One Star Restaurants": layers.onestar,
-    // "Two Stars Restaurants": layers.twostars,
-    // "Three Stars Restaurants": layers.threestars,
-    // "Four Stars Restaurants": layers.fourstars,  
-    // "Five Stars Restaurants": layers.fivestars
-};
-
-
-// Layer Control 
-L.control.layers(null, overlayMaps).addTo(myMap);
-
-// Legend
-
 // Call Data and Start Functions Setup
-d3.csv("yelpdata.csv", function (error, data) {
-    console.log(data)
+d3.csv("Data/yelpdata.csv", function (error, data) {
+    data = data.filter(data => data.city.includes("Las Vegas"))
+    console.log(data.length)
+
+    var onestar_markers = []
+    var twostar_markers = []
+    var threestar_markers = []
+    var fourstar_markers = []
+    var fivestar_markers = []
+
     data.forEach(data => {
-        var layer_star
         if (data.stars >= 1 & data.stars < 2){
-            layer_star = "onestar"
+            var newMarker = L.marker([data.latitude, data.longitude])
+            .bindPopup("Gordon Ramsey would be furious!");
+            onestar_markers.push(newMarker)
         }
-
-        var newMarker = L.marker([data.latitude, data.longitude]);
-
-        // Add the new marker to the appropriate layer
-        newMarker.addTo(layers[layer_star]);
-
-        // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-        newMarker.bindPopup("");
+        else if (data.stars >= 2 & data.stars < 3){
+            var newMarker = L.marker([data.latitude, data.longitude])
+            .bindPopup("");
+            twostar_markers.push(newMarker)
+        }
+        else if (data.stars >= 3 & data.stars < 4){
+            var newMarker = L.marker([data.latitude, data.longitude])
+            .bindPopup("");
+            threestar_markers.push(newMarker)
+        }
+        else if (data.stars >= 4 & data.stars < 5){
+            var newMarker = L.marker([data.latitude, data.longitude])
+            .bindPopup("");
+            fourstar_markers.push(newMarker)
+        }
+        else if (data.stars >= 5){
+            var newMarker = L.marker([data.latitude, data.longitude])
+            .bindPopup("Gordon Ramsey Approves!")
+            fivestar_markers.push(newMarker)
+        }
     }); 
+
+    
+    // Create a layer group made from the bike markers array, pass it into the createMap function
+    onestar_layer = L.layerGroup(onestar_markers.slice(50,500));
+    twostar_layer = L.layerGroup(twostar_markers.slice(50,500));
+
+    var streetsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
+
+    var baseMaps = {
+        "Streets Map": streetsMap,
+    };
+
+    // Define OverlayMaps
+    var overlayMaps = {
+        "One Star Restaurants": onestar_layer,
+        "Two Star Restaurants": twostar_layer,
+    };
+
+    // Create Map
+    var myMap = L.map("map", {
+        center: [41.114647, -115.172813], // Starting at Las Vegas
+        zoom: 5,
+        layers: [
+            streetsMap, 
+            onestar_layer,
+        ]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(myMap);
+
 });
-
-    // d3.csv("Data/Yelp_InitialScrub.csv", function (//city or rating))
-
-
