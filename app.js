399363b9f84497f7919dc6494b78cfcea9dc400e
Caitlin Donovan <caitlindonovan@caitlins-mbp.myfiosgateway.com>
2020-09-17 21:53:01 -0400

    adding final code for combo file

--- app.js
+++ app.js
@@ -2,173 +2,84 @@
 function init() {
     // select dropdown menu 
     var dropdown = d3.select("#selDataset");
-
-    // read the data 
-    d3.json("Data/samples.json").then((data)=> {
-        console.log(data)
-
-        // get the id data to the dropdwown menu
-        data.names.forEach(function(name) {
-            dropdown.append("option").text(name).property("value");
-        });
-
-        // call the functions to display the data and the plots to the page
-        getPlot(data.names[0]);
-        getInfo(data.names[0]);
+    var Cities = ["Toronto", "Charlotte", "Las Vegas", "Phoenix"]
+    Cities.forEach(function(mycity) {
+        dropdown.append("option").text(mycity).property("value", mycity);
     });
+    getPlot("Toronto")
+    getInfo("Toronto")
 }
 
 init();
 
-// Creating function for Data plotting (Bar, gauge, bubble)
-function getPlot(id) {
-    // getting data from the json file
-    d3.json("Data/samples.json").then((data)=> {
+// Creating function for Data plotting 
+function getPlot(mycity) {
+    // console.log(mycity)
+    // read the data 
+    d3.json("Data/Yelp_CitiesCount3.json").then((data)=> {
         console.log(data)
-  
-        var wfreq = data.metadata.map(d => d.wfreq)
-        console.log(`Washing Freq: ${wfreq}`)
+
+        // // get the city data to the dropdwown menu
+        var FilteredData = data.filter(filtercity => filtercity.city == mycity)
         
-        // filter sample values by id 
-        var samples = data.samples.filter(s => s.id.toString() === id)[0];
+        //Create bar plot
+        var Cuisines = FilteredData.map(data => data.categories)
+        console.log(Cuisines)
+
+        var CuisCount = FilteredData.map(data => data.business_id)
+        console.log(CuisCount)
+
+        var CityName = FilteredData.map(data => data.city)
+        console.log(CityName)
         
-        console.log(samples);
-  
-        // Getting the top 10 
-        var samplevalues = samples.sample_values.slice(0, 10).reverse();
-  
-        // get only top 10 otu ids for the plot OTU and reversing it. 
-        var OTU_top = (samples.otu_ids.slice(0, 10)).reverse();
-        
-        // get the otu id's to the desired form for the plot
-        var OTU_id = OTU_top.map(d => "OTU " + d)
-  
-      //   console.log(`OTU IDS: ${OTU_id}`)
-  
-  
-        // get the top 10 labels for the plot
-        var labels = samples.otu_labels.slice(0, 10);
-  
-      //   console.log(`Sample Values: ${samplevalues}`)
-      //   console.log(`Id Values: ${OTU_top}`)
-        // create trace variable for the plot
-        var trace = {
-            x: samplevalues,
-            y: OTU_id,
-            text: labels,
-            marker: {
-              color: 'rgb(142,124,195)'},
-            type:"bar",
-            orientation: "h",
-        };
-  
-        // create data variable
-        var data = [trace];
-  
-        // create layout variable to set plots layout
-        var layout = {
-            title: "Top 10 OTU",
-            yaxis:{
-                tickmode:"linear",
-            },
-            margin: {
-                l: 100,
-                r: 100,
-                t: 100,
-                b: 30
-            }
-        };
-  
-        // create the bar plot
-        Plotly.newPlot("bar", data, layout);
-  
-        //console.log(`ID: ${samples.otu_ids}`)
-      
-        // The bubble chart
+        // Create the Trace
         var trace1 = {
-            x: samples.otu_ids,
-            y: samples.sample_values,
-            mode: "markers",
-            marker: {
-                size: samples.sample_values,
-                color: samples.otu_ids
-            },
-            text: samples.otu_labels
-  
+            x: Cuisines,
+            y: CuisCount,
+            type: "bar"
         };
-  
-        // set the layout for the bubble plot
-        var layout_b = {
-            xaxis:{title: "OTU ID"},
-            height: 600,
-            width: 1000
+        
+        // Create the data array for the plot
+        var data = [trace1];
+        
+        // Define the plot layout
+        var layout = {
+            title: "Top Restaurants by Type",
+            xaxis: { title: "Cuisine Type" },
+            yaxis: { title: "Number of Restaurants" }
         };
-  
-        // creating data variable 
-        var data1 = [trace1];
-  
-        // create the bubble plot
-        Plotly.newPlot("bubble", data1, layout_b); 
-  
-        // The guage chart
-  
-        var data_g = [
-          {
-          domain: { x: [0, 1], y: [0, 1] },
-          value: parseFloat(wfreq),
-          title: { text: `Weekly Washing Frequency ` },
-          type: "indicator",
-          
-          mode: "gauge+number",
-          gauge: { axis: { range: [null, 9] },
-                   steps: [
-                    { range: [0, 2], color: "yellow" },
-                    { range: [2, 4], color: "cyan" },
-                    { range: [4, 6], color: "teal" },
-                    { range: [6, 8], color: "lime" },
-                    { range: [8, 9], color: "green" },
-                  ]}
-              
-          }
-        ];
-        var layout_g = { 
-            width: 700, 
-            height: 600, 
-            margin: { t: 20, b: 40, l:100, r:100 } 
-          };
-        Plotly.newPlot("gauge", data_g, layout_g);
-      });
-  }  
-// create the function to get the necessary data
-function getInfo(id) {
+        
+        // Plot the chart to a div tag with id "bar-plot"
+        Plotly.newPlot("bar", data, layout);
+            });
+
+};
+
+// Create the function to get the necessary data for the City Info
+function getInfo(mycity) {
     // read the json file to get data
-    d3.json("Data/samples.json").then((data)=> {
+    d3.json("Data/Yelp_CityFacts.json").then((data) => {
+        console.log(data)
+
+        // get the metadata info for the City Info
+        var citydata = data.filter(filtercity => filtercity.City == mycity);
+        console.log(citydata)
+
+        // select city to put data in field
+        var CityInfo = d3.select("#city-info");
         
-        // get the metadata info for the demographic panel
-        var metadata = data.metadata;
+        // empty the city info each time before getting new city info
+        CityInfo.html("");
 
-        console.log(metadata)
-
-        // filter meta data info by id
-        var result = metadata.filter(meta => meta.id.toString() === id)[0];
-
-        // select demographic panel to put data
-        var demographicInfo = d3.select("#sample-metadata");
-        
-        // empty the demographic info panel each time before getting new id info
-        demographicInfo.html("");
-
-        // grab the necessary demographic data data for the id and append the info to the panel
-        Object.entries(result).forEach((key) => {   
-                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
-        });
+        // // grab the necessary city data for each city and append the info to the panel
+        Object.entries(citydata[0]).forEach((key) => {   
+        CityInfo.append("h5").text(key[0] + ": " + key[1]);
+        })   
     });
-}
+};
 
 // create the function for the change event
-function optionChanged(id) {
-    getPlot(id);
-    getInfo(id);
+function optionChanged(mycity) {
+    getPlot(mycity);
+    getInfo(mycity);
 }
-
-
